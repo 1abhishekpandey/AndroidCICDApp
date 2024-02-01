08616e6a2a474c1cd452342c64597ed3657454b6
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-02-01 22:22:40 +0530

    Feat/123 (#6)
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * chore: just an empty commit
    
    * chore: just an empty commit
    
    * chore: just an empty commit
    
    * ci: add feature pipeline
    
    * chore: just an empty commit
    
    * Documentation/123 (#7)
    
    * chore: just an empty commit
    
    * chore: just an empty commit
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * chore: just an empty commit
    
    * chore: just an empty commit
    
    * chore: just an empty commit
    
    * chore: just an empty commit
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline
    
    * ci: add feature pipeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,20 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
+//// Create a variable called keystorePropertiesFile, and initialize it to your
+//// keystore.properties file, in the rootProject folder.
+//val keystorePropertiesFile = rootProject.file("keystore.properties")
+//
+//// Initialize a new Properties() object called keystoreProperties.
+//val keystoreProperties = Properties()
+//
+//// Load your keystore.properties file into the keystoreProperties object.
+//keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 android {
     namespace = "com.example.androidcicdapps"
@@ -17,8 +30,18 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+//    signingConfigs {
+//        create("config") {
+//            keyAlias = keystoreProperties["keyAlias"] as String
+//            keyPassword = keystoreProperties["keyPassword"] as String
+//            storeFile = file(keystoreProperties["storeFile"] as String)
+//            storePassword = keystoreProperties["storePassword"] as String
+//        }
+//    }
+
     buildTypes {
         release {
+//            signingConfig = signingConfigs.getByName("config")
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
